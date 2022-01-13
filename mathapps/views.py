97f8161aec2de748c318ce46764bc59bb6432b07
Kody-Timm <61067741+Kody-Timm@users.noreply.github.com>
2022-01-13 17:03:52 +0000

    Thu, Jan 13, 2022, 5:03 PM +00:00

--- mathapps/views.py
+++ mathapps/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from django.template import Context, loader
 
 def index(request):
-    template = loader.get_template("app/index.html")
-    return HttpResponse(template.render)
+    
+    return render(request, 'mathapps/index.html', context)
