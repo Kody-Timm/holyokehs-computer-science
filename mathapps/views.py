e8399d2f3b888298c8616b50f2ed43f9e66d91e1
Kody-Timm <61067741+Kody-Timm@users.noreply.github.com>
2022-01-19 10:31:56 -0700

    Update views.py

--- mathapps/views.py
+++ mathapps/views.py
@@ -4,4 +4,4 @@ from django.template import Context, loader
 
 def index(request):
     
-    return render(request, 'mathapps/templates/mathapps/index.html', context)
+    return render(request, 'index.html', context)
