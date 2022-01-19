dce68b24544d194754d1fe075e46fbf19b5c32c5
Kody-Timm <61067741+Kody-Timm@users.noreply.github.com>
2022-01-19 10:39:33 -0700

    Update views.py

--- mathapps/views.py
+++ mathapps/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.shortcuts import render
 from . import views
 
 def index(request):
