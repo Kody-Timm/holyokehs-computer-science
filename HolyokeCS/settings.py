2259253c88f4370053a1532c3f02b14524aa00eb
Kody-Timm <61067741+Kody-Timm@users.noreply.github.com>
2022-01-24 17:56:15 +0000

    changed Math

--- HolyokeCS/settings.py
+++ HolyokeCS/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'mathapps.apps.MathappsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
